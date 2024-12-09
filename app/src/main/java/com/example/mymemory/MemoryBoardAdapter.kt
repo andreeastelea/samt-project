a5d48e591eee5d12e4f120b0c1e4e0de30312d34
Andreea <andreeaandra1981@gmail.com>
2024-12-09 21:58:33 +0200

    First commit

---
+++ app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.mymemory
+import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
+import androidx.recyclerview.widget.RecyclerView.Recycler
+
+class MemoryBoardAdapter(private val context: Context,private val numPieces: Int) :
+
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+        companion object {
+            private const val MARGIN_SIZE = 10
+        }
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(position: Int) {
+
+
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cardWidth = parent.width/2 -(2*MARGIN_SIZE)
+        val cardHeight = parent.height/4-(2*MARGIN_SIZE)
+        val cardSideLength = min(cardWidth, cardHeight)
+        val view =LayoutInflater.from(context).inflate(R.layout.memory_card,parent, false)
+        val layoutParams =  view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MARGIN_SIZE,MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = numPieces
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(position)
+    }
+}
+
+
+
